e2e256193c9a2b770ea21ed8fd90c0d0debe8f98
Biliane Moreira <bilianemoreira@gmail.com>
2020-10-01 16:29:13 +0200

    send sqlite3 config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createRestTypeNode } from 'typescript';
+
 const app = express();
 
 app.use(express.json());
@@ -8,7 +8,6 @@ app.use(express.json());
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 
-}
+});
 
 app.listen(3333);
-
